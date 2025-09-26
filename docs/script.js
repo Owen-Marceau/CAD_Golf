bd73d53a52c088ff9748f53fcdb7ad9d0bab59d1
Owen Marceau <marceauowen@gmail.com>
2025-09-26 10:04:43 +0100

    new backed

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,7 @@
 let menuOpen = false;
 let playerDropActive = false;
 
-let url = "https://cad-golf.onrender.com"; // https://cad-golf.onrender.com
+let url = "https://cad-golf-yqiy.onrender.com"; // https://cad-golf.onrender.com
 let todayBox;
 let currentEvent;
 const months = [
